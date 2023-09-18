650b9c2e1bfb4213e04858151605397bcfb7d20b
kebtech <64436886+kebtech@users.noreply.github.com>
2023-09-18 12:33:04 -0400

    initial commit for cnctd core module

---
+++ target/package/cnctd-0.1.0/target/debug/deps/cnctd-a631ae7fb75cfebc.d
@@ -0,0 +1,7 @@
+/Users/kyleebner/Development/ConnectedDot/cnctd/cnctd.modules/rust/cnctd/target/package/cnctd-0.1.0/target/debug/deps/cnctd-a631ae7fb75cfebc.rmeta: src/lib.rs
+
+/Users/kyleebner/Development/ConnectedDot/cnctd/cnctd.modules/rust/cnctd/target/package/cnctd-0.1.0/target/debug/deps/libcnctd-a631ae7fb75cfebc.rlib: src/lib.rs
+
+/Users/kyleebner/Development/ConnectedDot/cnctd/cnctd.modules/rust/cnctd/target/package/cnctd-0.1.0/target/debug/deps/cnctd-a631ae7fb75cfebc.d: src/lib.rs
+
+src/lib.rs:
